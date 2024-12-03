2afba4b2385dd1bdd92c101a59cd3058486d85e2
Andrew Luxmore <aluxmore@telecost.com>
2024-12-03 15:30:06 -0500

    My website

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import {
   AboutSection,
-  ArticlesSection,
-  ContactSection,
+  // ArticlesSection,
+  // ContactSection,
   HeroSection,
   InterestsSection,
   Page,
@@ -16,11 +16,11 @@ export default function IndexPage() {
       <Seo title="Gatsby Starter for Portfolio Minimal" />
       <Page useSplashScreenAnimation>
         <HeroSection sectionId="hero" />
-        <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />
+        {/* <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} /> */}
         <AboutSection sectionId="about" heading="About Portfolio Minimal" />
         <InterestsSection sectionId="details" heading="Details" />
-        <ProjectsSection sectionId="features" heading="Built-in Features" />
-        <ContactSection sectionId="github" heading="Issues?" />
+        <ProjectsSection sectionId="projects" heading="Projects" />
+        {/* <ContactSection sectionId="github" heading="Github" /> */}
       </Page>
     </>
   );
